cceedd6eba4d007b01cb32fe5610201c1faef37b
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-07-06 17:06:41 +0300

    Add files via upload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,82 +1,98 @@
 #!/usr/bin/python3
-"""Database storage"""
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
+""" This modules handles Database Storage """
+from sqlalchemy import create_engine
+from os import getenv
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.user import User
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker, relationship, scoped_session
-from os import getenv
+from models.amenity import Amenity
 
 
 class DBStorage:
-    """Database storage class
-    Attributes: __engine, __session
-    """
+    '''
+    Handles database engine
+    '''
     __engine = None
     __session = None
 
     def __init__(self):
-        """Creates the engine"""
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(getenv('HBNB_MYSQL_USER'),
-                                              getenv('HBNB_MYSQL_PWD'),
-                                              getenv('HBNB_MYSQL_HOST'),
-                                              getenv('HBNB_MYSQL_DB')),
-                                      pool_pre_ping=True)
+        '''
+        Create engine for database
+        '''
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True
+        )
+
         if getenv('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Method that queries on the currect database session"""
-        objects_dictionary = {}
+        '''
+        query for all objects on the current database session
+        '''
+        classes = {
+            "City": City,
+            "State": State,
+            "User": User,
+            "Place": Place,
+            "Review": Review,
+            "Amenity": Amenity,
+        }
+        result = {}
+        query_rows = []
 
-        if cls is None:
-            objects_list = self.__session.query(State).all()
-            objects_list.extend(self.__session.query(City).all())
-            objects_list.extend(self.__session.query(User).all())
-            objects_list.extend(self.__session.query(Place).all())
-            objects_list.extend(self.__session.query(Review).all())
-            objects_list.extend(self.__session.query(Amenity).all())
-        else:
-            if type(cls) == str:
+        if cls:
+            '''Query for all objects belonging to cls'''
+            if type(cls) is str:
                 cls = eval(cls)
-            objects_list = self.__session.query(cls).all()
-
-        for obj in objects_list:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            objects_dictionary[key] = obj
-
-        return objects_dictionary
+            query_rows = self.__session.query(cls)
+            for obj in query_rows:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                result[key] = obj
+            return result
+        else:
+            '''Query for all types of objects'''
+            for name, value in classes.items():
+                query_rows = self.__session.query(value)
+                for obj in query_rows:
+                    key = '{}.{}'.format(name, obj.id)
+                    result[key] = obj
+            return result
 
     def new(self, obj):
-        """Method that adds the object to the current database session"""
+        '''add the object to the current database session'''
         self.__session.add(obj)
 
     def save(self):
-        """Method that commits all changes of the current database session"""
-
+        '''commit all changes of the current database session'''
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Method that deletes from the current database
-        session obj if not None"""
-
-        if obj:
-            self.__session.delete(obj)
+        '''delete obj from the current database session'''
+        self.__session.delete(obj)
 
     def reload(self):
-        """Method that creates all tables in the database"""
-
+        '''
+        - create all tables in the database
+        - create the current database session from the engine
+        '''
         Base.metadata.create_all(self.__engine)
-        my_session = sessionmaker(bind=self.__engine,
-                                  expire_on_commit=False)
-        Session = scoped_session(my_session)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """Method that closes the session"""
+        """
+        Because SQLAlchemy doesn't reload his `Session`
+        when it's time to insert new data, we force it to!
+        """
         self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -17,15 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        filtered_by_class = {}
         if cls:
-            if type(cls) == str:
-                cls = eval(cls)
-            my_dict = {}
-            for key, value in self.__objects.items():
-                if type(value) == cls:
-                    my_dict[key] = value
-            return my_dict
-        return self.__objects
+            for key, value in FileStorage.__objects.items():
+                if value.__class__ == cls:
+                    filtered_by_class[key] = value
+            return filtered_by_class
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -42,12 +33,19 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -58,13 +56,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """Deletes and object from __objects"""
-        if (obj):
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
+        ''' delete obj from __objects if it is inside '''
+        if obj:
+            key = '{}.{}'.format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
 
     def close(self):
-        """Calls reload method for deserializing
-        the JSON file to objects
-        """
+        """ Deserialize JSON file to objects before leaving """
         self.reload()
